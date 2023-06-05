2f5ac8501b8137d450b59050e2491e1ecf1f9451
Daniel Ngugi <danyhaze@gmail.com>
2023-06-05 22:44:25 +0300

    First js assignment

---
+++ script.js
@@ -0,0 +1,45 @@
+/* // A single line comment has two forward slashes
+/* This is how to write multiple lines
+Such as these*/
+/*
+console.log ("this is what appears")
+console.log (2+3)
+console.log ("Javascript is abbreviated as js")
+console.log (60==59)
+console.log (60 > 45) ;
+var name = "Mwenye Nguvu";
+console.log (name) ;
+
+var age = "50"
+console.log(age);
+var town = "Nakuru"
+
+console.log ("My name is So and So")
+
+console.log ("His name is " + name + " from " + town + " and I'm " + age + " centuries old.")
+
+var First_Name = "Stan"
+var Last_Name = "Lee" 
+
+console.log ("His full name is " + First_Name + " " +  Last_Name)
+
+var sum = 2+2;
+console.log(sum) ;
+
+
+var Gender1 = "His"
+var Gender2 = "Her"
+var First_Name = "Snow"
+var Second_Name = "Alegra"
+console.log (Gender2 + " name is "  + First_Name + " " + Second_Name) ;
+ */
+var x = 2
+var y = 5 
+var sum = x+y ;
+var difference = x-y ;
+var product = x*y ;
+var quotient = x/y ;
+var modulus = x%y ;
+console.log (modulus) ;
+ 
+Number.isInteger(x, y);
