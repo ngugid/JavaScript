e21a56a62c752789c77b459f30c7a9872a77c4ee
Daniel Ngugi <danyhaze@gmail.com>
2023-06-10 10:12:35 +0300

    third commit

--- day2.js
+++ day2.js
@@ -7,15 +7,16 @@
    console.log(perimeter);
 
    let area = l*w;
-   console.log(area);*/
-/*
+   console.log(area);
+
+   //Quiz Day 2
 let number = 17;
 if( number % 3 === 0 || number % 5 === 0); {
 console.log (number + " divisible by 3 or 5");
 }
  else (number % 3 !=== 0 || number % 5 !== 0);{
     console log (number + " NOT divisible by 3 or 5")
- }*/
+ }
 
  //Quiz Day 2
 let num = 7;
@@ -26,15 +27,30 @@ while(count<=num){
     count++;
 }console.log(sum);
 
+
 for (let num = 1; num <= 1; num++);
 console.log(num)
 
 if(num % 3 === 0 || num % 5 === 0){
     console.log(num + " divisible by 3 or 5");
-}else{
-    console.log (num + " not divisible by 3 or 5");
-}
-   
+}*/
+   //Try 2
 
-
-
+ 
+   function sumofnumbers(n) {
+    let sum = 0;
+  
+    for (let i = 1; i <= n; i++) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+      }
+    }
+  
+    return sum;
+  }
+  
+  
+  const n = 15;
+  const result = sumofnumbers(n);
+  console.log(`The sum of numbers between 1 and ${n} divisible by 3 or 5 is: ${result}`);
+  
---
+++ day3.js
@@ -0,0 +1,86 @@
+/*
+for(let i = 1; i <= 100; i++){
+    if (i % 2 ===0){
+        console.log(i + " divisible by 2")
+    }
+}    */
+
+
+/*
+function sumOf1toN(n) {
+    let sum = 0;
+    for(let i = 1; i<=n; i++){
+    if (i %  3 === 0 || i % 5 === 0){
+    sum = sum +i;
+    console.log(i)
+        }
+    }
+}
+
+//Objects
+
+let person = {
+    name: "James",
+    age: 32,
+    occupation: "dev",
+    status: "Married",
+    family:{
+        wife: "Samy",
+         son: "Jr",
+        daughter: "Nel",
+    }
+}
+
+console.log(person.name)
+
+
+let number = 17;
+if( number % 3 === 0 || number % 5 === 0); {
+console.log (number + " divisible by 3 or 5");
+}
+ else( number % 3 !== 0 || number % 5 !== 0);{
+    console log (number + " NOT divisible by 3 or 5")
+ }
+ 
+let num = 17
+ if(num % 3 === 0 || num % 5 === 0){
+    console.log(num + " divisible by 3 or 5");
+} else {
+console.log(num + " NOT divisible")
+}
+
+function log(num){
+    if(num > 2){
+        return;
+    }
+    console.log(num);
+    log(num + 1);
+}
+
+log(1);
+
+
+for(let i = 1; i <= 100; i++){
+    if (i % 2 ===0){
+        console.log(i + " divisible by 2")
+    }
+}  
+
+let groupBy = [
+    {name: 'Andy', age:35, job: "writer", preference: "noPepper", payment: "creditcard"}
+    { name: "Anne", age:25, job: "author", preference: "ketchup", payment: "cash"}
+    { name: "Jim", age:27, job: "driver", preference: "pepper", payment: "cash"}
+    { name: "John", age:50, job: "mechanic", preference: "extraPepper", payment: "creditcard"}
+    { name: "Andrew", age:30, job: "artist", preference: "mayo", payment: "coupon"}
+];
+
+let groupBy =[
+ {name: "Andy", age: 35},
+ {name: "Anne", age:25},
+ {name: "Jim", age:27},
+ {name: "John", age:50},
+{name: "Andrew", age:30} 
+];
+
+console.log(groupBy.age) */
+
